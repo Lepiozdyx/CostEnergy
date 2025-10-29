f2c4da220dee6e238c14d78f000cf624b71a38f1
Developer <Lepiozdyx@gmail.com>
2025-10-29 15:21:50 -0300

    Initial Commit

---
+++ CostEnergy/CostEnergyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CostEnergyApp.swift
+//  CostEnergy
+//
+//  Created by Alex on 29.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct CostEnergyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
